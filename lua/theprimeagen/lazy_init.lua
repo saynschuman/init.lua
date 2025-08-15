e1bc2b2e845b006918c2940a70d371ae2b1d1ae7
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-15 03:24:03 -0700

    Disable local plugins and luarocks

--- lua/theprimeagen/lazy_init.lua
+++ lua/theprimeagen/lazy_init.lua
@@ -13,5 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = "theprimeagen.lazy",
-    change_detection = { notify = false }
+    change_detection = { notify = false },
+    rocks = { enabled = false }
 })
